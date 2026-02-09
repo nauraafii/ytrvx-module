7132d3c88f25bc1ff3424301e10e50ef5afd3248
Naufa <afifiabrar778@gmail.com>
2026-02-09 08:39:11 +0700

    Update

--- build.sh
+++ build.sh
@@ -168,7 +168,7 @@ if [ -z "$(ls -A1 "${BUILD_DIR}")" ]; then abort "All builds failed."; fi
 
 log "\nInstall [Microg](https://github.com/ReVanced/GmsCore/releases) or [Microg-RE](https://github.com/MorpheApp/MicroG-RE/releases) for non-root YouTube and YT Music APKs"
 log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) to detach root YouTube and YT Music from Play Store"
-log "\n[yt-morphe](https://github.com/nauraafii/yt-morphe)\n"
+log "\n[ytrvx-module](https://github.com/nauraafii/ytrvx-module)\n"
 log "$(cat "$TEMP_DIR"/*/changelog.md)"
 
 SKIPPED=$(cat "$TEMP_DIR"/skipped 2>/dev/null || :)
